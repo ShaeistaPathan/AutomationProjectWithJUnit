e9dd8d7d910acdac596913f88e751a54b438a5e4
ShaeistaPathan <102866364+ShaeistaPathan@users.noreply.github.com>
2022-06-30 01:20:22 -0500

    commit3

---
+++ src/main/java/Pages/ToDoListPage.java
@@ -0,0 +1,120 @@
+package Pages;
+
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+import junit.framework.Assert;
+
+public class ToDoListPage {
+
+
+	WebDriver driver;
+	
+	public ToDoListPage (WebDriver driver) {
+		this.driver = driver;
+	}	
+	    @FindBy(how= How.NAME,using="data") WebElement ListFieldElement;
+	    @FindBy(how= How.CSS,using="input[value='Add']") WebElement AddButtonElement;
+		@FindBy(how= How.NAME,using="allbox") WebElement ToggleAllCheckboxElement;
+		@FindBy(how= How.NAME,using="todo[0]") WebElement FirstListCheckboxElement;
+		@FindBy(how= How.NAME,using="todo[1]") WebElement SecondListCheckboxElement;
+		@FindBy(how= How.NAME,using="todo[2]") WebElement ThirdListCheckboxElement;
+		@FindBy(how= How.CSS,using="input[value='Remove']") WebElement RemoveButtonElement;
+	    
+		public void addOneListItem() {
+			ListFieldElement.sendKeys("Study");
+			AddButtonElement.click();
+		}
+		public void addThreeListItems() {
+			ListFieldElement.sendKeys("Study");
+			AddButtonElement.click();
+			ListFieldElement.sendKeys("Homework");
+			AddButtonElement.click();
+			ListFieldElement.sendKeys("Dinner");
+			AddButtonElement.click();
+			
+		}
+		
+		public void clickRemoveButton() {
+			RemoveButtonElement.click();
+		}
+		public void checkToggleAllCheckbox() {
+			ToggleAllCheckboxElement.click();
+		}
+
+        public void validateToggleAllWithListCheckboxes() {
+        	Assert.assertEquals(ToggleAllCheckboxElement.isSelected(),FirstListCheckboxElement.isSelected()&&SecondListCheckboxElement.isSelected()&&ThirdListCheckboxElement.isSelected());
+        }
+
+//        public void validateToggleAllWithSecondListCheckbox() {
+//        	Assert.assertEquals(ToggleAllCheckboxElement.isSelected(),SecondListCheckboxElement.isSelected());
+//        }
+//        
+//        public void validateToggleAllWithThirdListCheckbox() {
+//        	Assert.assertEquals(ToggleAllCheckboxElement.isSelected(),ThirdListCheckboxElement.isSelected());
+//        }
+        
+        public void DeleteFirstItemWithSingleCheckboxAndRemoveButton() {
+        	FirstListCheckboxElement.click();
+        	RemoveButtonElement.click();   
+        }
+        
+        public void validateDeleteFirstItemWithSingleCheckboxAndRemoveButton() {
+       	   Assert.assertTrue("List item is present!", FirstListCheckboxElement!=null);
+//        	try {
+//        		FirstListCheckboxElement.isDisplayed();
+//        		System.out.println("First list item is present.");
+//        	}catch(NoSuchElementException e) {
+//        		System.out.println("First list item is deleted.");
+//        	}
+        }
+        
+        public void validateSecondAndThirdListItemIsPresent() {
+        	Assert.assertTrue(SecondListCheckboxElement.isDisplayed()&&ThirdListCheckboxElement.isDisplayed());
+        }
+        
+        
+
+         public void validateDeleteSecondItemWithSingleCheckboxAndRemoveButton() {
+             	try {
+            		SecondListCheckboxElement.isDisplayed();
+            		System.out.println("Second list item is present.");
+            	}catch(NoSuchElementException e) {
+            		System.out.println("Second list item is deleted.");
+            	}
+         }
+          public void validateDeleteThirdItemWithSingleCheckboxAndRemoveButton() {
+               	try {
+                		ThirdListCheckboxElement.isDisplayed();
+                		System.out.println("Third list item is present.");
+                	}catch(NoSuchElementException e) {
+                		System.out.println("Third list item is deleted.");
+                	}
+        }
+         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+}
---
+++ src/test/java/test/ToDoListTest.java
@@ -0,0 +1,61 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import Pages.ToDoListPage;
+import util.BrowserFactory;
+
+public class ToDoListTest {
+
+	WebDriver driver;
+	ToDoListPage toDoListPageObj;
+	
+	@Before
+	public void setUp() {
+		driver = BrowserFactory.inIt();
+	}
+	
+//	@Test
+	public void validateToggleAllCheckboxWithListItemsCheckbox() {
+	    toDoListPageObj = PageFactory.initElements(driver, ToDoListPage.class);
+	    toDoListPageObj.addThreeListItems();
+		toDoListPageObj.checkToggleAllCheckbox();
+		toDoListPageObj.validateToggleAllWithListCheckboxes();
+	
+		toDoListPageObj.clickRemoveButton();
+	}
+	
+	@Test
+	public void validateSingleItemRemovalWithSingleCheckboxAndRemoveButton() {
+		toDoListPageObj = PageFactory.initElements(driver, ToDoListPage.class);
+		toDoListPageObj.addThreeListItems();;
+		toDoListPageObj.DeleteFirstItemWithSingleCheckboxAndRemoveButton();
+		toDoListPageObj.validateDeleteFirstItemWithSingleCheckboxAndRemoveButton();
+		toDoListPageObj.validateSecondAndThirdListItemIsPresent();
+	}
+	
+//	@Test
+	public void ValidateDeleteAllListItemsWithRemoveButtonAndToggleAllOn() {
+		toDoListPageObj = PageFactory.initElements(driver, ToDoListPage.class);
+		toDoListPageObj.addThreeListItems();
+		toDoListPageObj.checkToggleAllCheckbox();
+		toDoListPageObj.clickRemoveButton();
+		toDoListPageObj.validateDeleteFirstItemWithSingleCheckboxAndRemoveButton();
+		toDoListPageObj.validateDeleteSecondItemWithSingleCheckboxAndRemoveButton();
+		toDoListPageObj.validateDeleteThirdItemWithSingleCheckboxAndRemoveButton();
+		
+	}
+	
+//	@After
+	public void closeBrowser() {
+		BrowserFactory.tearDown();
+	}
+	
+	
+	
+	
+}
